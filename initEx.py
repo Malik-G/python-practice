48f7cbb1bc9646d39f8d259d243d3db8152788ac
Malik G <Malik@Malik.local>
2018-04-17 13:03:58 -0500

    Shows the functionality of an init method inside of a class

---
+++ initEx.py
@@ -0,0 +1,13 @@
+
+class student:
+
+	def __init__(self, name, grade, age):
+		self.name = name
+		self.grade = grade
+		self.age = age
+
+ralph = student("Ralph", 80, 20)
+damon = student("Damon", 90, 19)
+kelly = student("Kelly", 98, 18)
+
+print (ralph.grade)
